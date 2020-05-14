b02f34b922873aa95aa2effcbe9022dc0676777f
dkumazaw <dkumazaw@stanford.edu>
2020-05-13 21:10:56 -0700

    Change phrasing

--- init.sh
+++ init.sh
@@ -4,9 +4,8 @@
 SHELLRC="$HOME/.bashrc"
 # -----------------------------
 
-
 # Setup vimrc
-echo 'Setting up vim'
+echo 'Set up vim'
 git clone --depth=1 https://github.com/dkumazaw/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
